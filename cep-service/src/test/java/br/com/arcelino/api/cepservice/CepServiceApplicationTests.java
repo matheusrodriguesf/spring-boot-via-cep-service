a5286d788d17c59cf8c7f8cec19e18dd951cf746
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2023-04-20 16:40:14 -0300

    Import Project

---
+++ cep-service/src/test/java/br/com/arcelino/api/cepservice/CepServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arcelino.api.cepservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
